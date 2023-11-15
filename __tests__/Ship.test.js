8a9aa891bc23a58b9ee51a8f580dc5b6ab1e1c1f
Chris O'Neill <chris_oslow@hotmail.com>
2023-11-15 21:22:46 +0000

    Ship itinerary test and functionality coded added. Refactored Ship test suites

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -1,24 +1,32 @@
 const Ship = require("../src/Ship");
 const Port = require("../src/Port");
+const Itinerary = require("../src/Itinerary");
 
 describe("Ship", () => {
   it("can be instantiated", () => {
-    expect(new Ship()).toBeInstanceOf(Object);
+    const port = new Port("Dover");
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
+
+    expect(ship).toBeInstanceOf(Object);
   });
 
   it("has a starting port", () => {
     const port = new Port("Dover");
-    const ship = new Ship(port);
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
 
     expect(ship.currentPort).toBe(port);
   });
 
   it("can dock at a different port", () => {
     const dover = new Port("Dover");
-    const ship = new Ship(dover);
-
     const calais = new Port("Calais");
-    ship.dock(calais);
+    const itinerary = new Itinerary([dover, calais]);
+    const ship = new Ship(itinerary);
+
+    ship.setSail();
+    ship.dock();
 
     expect(ship.currentPort).toBe(calais);
   });
@@ -26,11 +34,25 @@ describe("Ship", () => {
 
 describe("setSail", () => {
   it("can set sail", () => {
-    const port = new Port("Dover");
-    const ship = new Ship(port);
+    const dover = new Port("Dover");
+    const calais = new Port("Calais");
+    const itinerary = new Itinerary([dover, calais]);
+    const ship = new Ship(itinerary);
 
     ship.setSail();
 
     expect(ship.currentPort).toBeFalsy();
+  });
+
+  it("can't sail further than its itinerary", () => {
+    const dover = new Port("Dover");
+    const calais = new Port("Calais");
+    const itinerary = new Itinerary([dover, calais]);
+    const ship = new Ship(itinerary);
+
+    ship.setSail();
+    ship.dock();
+
+    expect(() => ship.setSail()).toThrowError("End of itinerary reached");
   });
 });
